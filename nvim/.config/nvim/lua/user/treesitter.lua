a45219cc9852cdf8f03291ec98aec350f8dc8db9
Tolga Gonul <tolgon@gmail.com>
2022-09-09 18:01:51 +0200

    Changed rainbow colors for brackets.

--- nvim/.config/nvim/lua/user/treesitter.lua
+++ nvim/.config/nvim/lua/user/treesitter.lua
@@ -5,10 +5,11 @@ end
 
 configs.setup({
 	ensure_installed = "all", -- one of "all" or a list of languages
+  sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
 	ignore_install = { "" }, -- List of parsers to ignore installing
 	highlight = {
 		enable = true, -- false will disable the whole extension
-		disable = { "" }, -- list of language that will be disabled
+		disable = { "css" }, -- list of language that will be disabled
 	},
 	autopairs = {
 		enable = true,
@@ -20,7 +21,14 @@ configs.setup({
     -- disable = { "html" }, -- list of languages you want to disable the plugin for
     extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
     max_file_lines = nil, -- Do not enable for files with more than n lines, int
-    -- colors = {}, -- table of hex strings
+    colors = {
+      "MediumOrchid",
+      "Gold",
+      "LightSkyBlue",
+      "LightGreen",
+      "Tomato",
+      "DarkOrchid",
+    }, -- table of hex strings
     -- termcolors = {} -- table of colour name strings
   }
 })
