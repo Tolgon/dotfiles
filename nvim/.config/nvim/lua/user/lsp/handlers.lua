88bcfffcd5e7a1e68718b51f900544b6374df2b7
Tolga Gonul <tolgon@gmail.com>
2022-11-08 11:45:31 +0100

    Changed some navic icons to work with PragmataPro Mono

--- nvim/.config/nvim/lua/user/lsp/handlers.lua
+++ nvim/.config/nvim/lua/user/lsp/handlers.lua
@@ -90,6 +90,34 @@ M.on_attach = function(client, bufnr)
     if client.server_capabilities.documentSymbolProvider then
         local navic = require "nvim-navic"
         navic.setup {
+            icons = {
+                File          = " ",
+                Module        = " ",
+                Namespace     = " ",
+                Package       = " ",
+                Class         = " ",
+                Method        = " ",
+                Property      = " ",
+                Field         = " ",
+                Constructor   = " ",
+                Enum          = "練",
+                Interface     = "練",
+                Function      = " ",
+                Variable      = " ",
+                Constant      = " ",
+                String        = " ",
+                Number        = " ",
+                Boolean       = "◩ ",
+                Array         = " ",
+                Object        = " ",
+                Key           = " ",
+                Null          = "ﳠ ",
+                EnumMember    = " ",
+                Struct        = " ",
+                Event         = " ",
+                Operator      = " ",
+                TypeParameter = " ",
+            },
             highlight = true
         }
         navic.attach(client, bufnr)
