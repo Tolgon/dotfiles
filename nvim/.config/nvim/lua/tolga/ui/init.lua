3b2f8e3652670e0721fa88d7e6a2e552420298ca
Tolga Gonul <tolgon@gmail.com>
2023-06-13 07:53:00 +0200

    some fixes

--- nvim/.config/nvim/lua/tolga/ui/init.lua
+++ nvim/.config/nvim/lua/tolga/ui/init.lua
@@ -8,24 +8,24 @@ require("tolga.ui.notify")
 require("tolga.ui.navic")
 
 -- Remove alacritty padding on neovim
-function Sad(line_nr, from, to, fname)
-	vim.cmd(string.format("silent !sed -i '%ss/%s/%s/' %s", line_nr, from, to, fname))
-end
-
-function IncreasePadding()
-	Sad("10", 0, 10, "~/.dotfiles/alacritty/.config/alacritty/alacritty.yml")
-	Sad("11", 0, 5, "~/.dotfiles/alacritty/.config/alacritty/alacritty.yml")
-end
-
-function DecreasePadding()
-	Sad("10", 10, 0, "~/.dotfiles/alacritty/.config/alacritty/alacritty.yml")
-	Sad("11", 5, 0, "~/.dotfiles/alacritty/.config/alacritty/alacritty.yml")
-end
-
-vim.cmd([[
-  augroup ChangeAlacrittyPadding
-   au! 
-   au VimEnter * lua DecreasePadding()
-   au VimLeavePre * lua IncreasePadding()
-  augroup END 
-]])
+-- function Sad(line_nr, from, to, fname)
+-- 	vim.cmd(string.format("silent !sed -i '%ss/%s/%s/' %s", line_nr, from, to, fname))
+-- end
+--
+-- function IncreasePadding()
+-- 	Sad("10", 0, 10, "~/.dotfiles/alacritty/.config/alacritty/alacritty.yml")
+-- 	Sad("11", 0, 5, "~/.dotfiles/alacritty/.config/alacritty/alacritty.yml")
+-- end
+--
+-- function DecreasePadding()
+-- 	Sad("10", 10, 0, "~/.dotfiles/alacritty/.config/alacritty/alacritty.yml")
+-- 	Sad("11", 5, 0, "~/.dotfiles/alacritty/.config/alacritty/alacritty.yml")
+-- end
+--
+-- vim.cmd([[
+--   augroup ChangeAlacrittyPadding
+--    au! 
+--    au VimEnter * lua DecreasePadding()
+--    au VimLeavePre * lua IncreasePadding()
+--   augroup END 
+-- ]])
