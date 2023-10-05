6be969dea5836353e7c21f78750f58e6fb84f90f
Tolga Gonul <tolgon@gmail.com>
2023-10-05 13:56:12 +0200

    upgrade indent-blankline v3, make config working, will add new opts later

--- nvim/.config/nvim/lua/tolga/ui/indent-blankline.lua
+++ nvim/.config/nvim/lua/tolga/ui/indent-blankline.lua
@@ -1,4 +1,4 @@
-local status_ok, indent_blankline = pcall(require, "indent_blankline")
+local status_ok, indent_blankline = pcall(require, "ibl")
 if not status_ok then
 	return
 end
@@ -43,25 +43,14 @@ vim.g.indent_blankline_context_patterns = {
 	"import_statement",
 	"operation_type",
 }
--- HACK: work-around for https://github.com/lukas-reineke/indent-blankline.nvim/issues/59
-vim.wo.colorcolumn = "99999"
 
--- vim.cmd [[highlight IndentBlanklineIndent1 guifg=#E06C75 gui=nocombine]]
--- vim.cmd [[highlight IndentBlanklineIndent2 guifg=#E5C07B gui=nocombine]]
--- vim.cmd [[highlight IndentBlanklineIndent3 guifg=#98C379 gui=nocombine]]
--- vim.cmd [[highlight IndentBlanklineIndent4 guifg=#56B6C2 gui=nocombine]]
--- vim.cmd [[highlight IndentBlanklineIndent5 guifg=#61AFEF gui=nocombine]]
--- vim.cmd [[highlight IndentBlanklineIndent6 guifg=#C678DD gui=nocombine]]
--- vim.opt.list = true
--- vim.opt.listchars:append "space:⋅"
--- vim.opt.listchars:append "space:"
--- vim.opt.listchars:append "eol:↴"
+vim.wo.colorcolumn = "99999"
 
 indent_blankline.setup({
 	-- show_end_of_line = true,
-	space_char_blankline = " ",
-	show_current_context = true,
-	show_current_context_start = true,
+	-- space_char_blankline = " ",
+	-- show_current_context = true,
+	-- show_current_context_start = true,
 	-- char_highlight_list = {
 	--   "IndentBlanklineIndent1",
 	--   "IndentBlanklineIndent2",
