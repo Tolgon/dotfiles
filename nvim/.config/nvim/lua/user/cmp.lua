3f083ef6ef505ad74321b3f5707441359282b7c0
Tolga Gonul <tolgon@gmail.com>
2024-02-01 14:43:58 +0100

    neotabs

--- nvim/.config/nvim/lua/user/cmp.lua
+++ nvim/.config/nvim/lua/user/cmp.lua
@@ -84,11 +84,9 @@ function M.config()
         elseif luasnip.expand_or_jumpable() then
           luasnip.expand_or_jump()
         elseif check_backspace() then
-          fallback()
-          -- require("neotab").tabout()
+          require("neotab").tabout()
         else
-          fallback()
-          -- require("neotab").tabout()
+          require("neotab").tabout()
         end
       end, {
         "i",
