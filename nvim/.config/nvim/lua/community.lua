02c75ef4a037bfc0c9021e4cf80e50e82f0bfa6c
Tolga Gonul <tolgon@gmail.com>
2024-05-25 11:28:56 +0200

    reorganize

---
+++ nvim/.config/nvim/lua/community.lua
@@ -0,0 +1,18 @@
+-- AstroCommunity: import any community modules here
+-- We import this file in `lazy_setup.lua` before the `plugins/` folder.
+-- This guarantees that the specs are processed before any user plugins.
+
+---@type LazySpec
+return {
+  "AstroNvim/astrocommunity",
+  { import = "astrocommunity.pack.lua" },
+  -- import/override with your plugins folder
+  { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.colorscheme.iceberg" },
+  -- language support
+  { import = "astrocommunity.pack.typescript-all-in-one" },
+  { import = "astrocommunity.pack.html-css" },
+  { import = "astrocommunity.pack.cs" },
+  -- misc
+  { import = "astrocommunity.scrolling.neoscroll-nvim" },
+}
